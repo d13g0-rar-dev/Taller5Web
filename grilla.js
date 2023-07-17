3d6c8d569bc97d10985bd4c3553ce0b998590f19
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2023-07-17 14:57:19 -0500

    Grilla Terminada

--- grilla.js
+++ grilla.js
@@ -1,39 +1,46 @@
-fetch('Taller5Web/users.json')
-  .then(response => response.json())
-  .then(data => {
-    // Aquí puedes manipular los datos obtenidos del archivo JSON
-    generarTabla(data);
+$(document).ready(function() {
+
+  $.ajax({
+      url: "https://jsonplaceholder.typicode.com/users",
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+          
+          var tabla = $("#grilla").jqGrid({
+              datastr: data,
+              datatype: "jsonstring",
+              jsonReader: {
+                repeatitems: false
+              },
+              colModel: [
+                { label: "Id", name: "id", width: 300 },
+                { label: "Nombre", name: "name", width: 300 },
+                { label: "Usuario", name: "username", width: 300 },
+                { label: "Email", name: "email", width: 300 },
+                { label: "Calle", name: "address.street", width: 300 },
+                { label: "Suite", name: "address.suite", width: 300 },
+                { label: "Ciudad", name: "address.city", width: 300 },
+                { label: "Codigo ZIP", name: "address.zipcode", width: 300 },
+                { label: "Latitud", name: "address.geo.lat", width: 300 },
+                { label: "Longitud", name: "address.geo.lng", width: 300 },
+                { label: "Telefono", name: "phone", width: 300 },
+                { label: "Sitio web", name: "website", width: 300 },
+                { label: "Nombre compania", name: "company.name", width: 300 },
+                { label: "Catch Phrase", name: "company.catchPhrase", width: 300 },
+                { label: "Bs", name: "company.bs", width: 300 }
+              ],
+              height: 3000,
+              width: 4000
+            });
+
+                    
+            $("#filtrarNombre").click(function() {
+              tabla.jqGrid('filterToolbar', { defaultSearch: 'cn' });
+          });
+      }
+  
   });
 
-
-  function generarTabla(data) {
-    // Obtén la referencia del elemento HTML donde deseas insertar la tabla
-    var tablaContainer = document.getElementById('tabla-container');
-  
-    // Crea la tabla y su encabezado
-    var tabla = document.createElement('table');
-    var encabezado = tabla.createTHead();
-    var filaEncabezado = encabezado.insertRow();
-  
-    // Recorre las claves del primer objeto para crear las columnas del encabezado
-    for (var key in data[0]) {
-      var th = document.createElement('th');
-      th.innerHTML = key;
-      filaEncabezado.appendChild(th);
-    }
-  
-    // Crea las filas de datos
-    var cuerpo = tabla.createTBody();
-    data.forEach(function(obj) {
-      var fila = cuerpo.insertRow();
-      for (var key in obj) {
-        var celda = fila.insertCell();
-        celda.innerHTML = obj[key];
-      }
-    });
-  
-    // Inserta la tabla en el contenedor
-    tablaContainer.appendChild(tabla);
-  }
+});
 
   
