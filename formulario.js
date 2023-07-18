c3bd68791a318b063e6fc39b61910585b05e32bc
desgrac1aaa <decire2211835@correo.uis.edu.co>
2023-07-17 21:06:48 -0500

    commit

--- formulario.js
+++ formulario.js
@@ -1,189 +1,148 @@
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
-
-const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo.
-	ccusuario: /^[a-zA-Z0-9]{10,20}$/,
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,25}$/, // Letras y espacios, pueden llevar acentos.
-	apellido: /^[a-zA-ZÀ-ÿ\s]{1,25}$/, //Letras y espacios, pueden llevar acentos.
-	direccion:/\b(^(cll|cra|av|anv|trans))\b/i,
-	password: /^[a-zA-Z0-9#%/&]{15,20}$/, // 15 a 20 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-}
-
-const campos = {
-	usuario: false,
-	ccusuario: false,
-	nombre: false,
-	apellido: false,
-	direccion: false,
-	password: false,
-	correo: false,
-}
-
-const validarFormulario = (e) => {
-	switch (e.target.name) {
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
-		break;
-		case "ccusuario":
-			validarCampo(expresiones.ccusuario, e.target, 'ccusuario');
-		break;
-		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'nombre');
-		break;
-		case "apellido":
-			validarCampo(expresiones.apellido, e.target, 'apellido');
-		break;
-		case "direccion":
-			validarCampo(expresiones.direccion, e.target, 'direccion');
-		break;
-		case "password":
-			validarCampo(expresiones.password, e.target, 'password');
-			validarPassword2();
-		break;
-		case "password2":
-			validarPassword2();
-		break;
-		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
-		break;
-	}
-}
-
-const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos[campo] = true;
-	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos[campo] = false;
-	}
-}
-
-const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
-
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
-	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-}
-
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.ccusuario && campos.nombre && campos.apellido && campos.direccion && campos.password && campos.correo && terminos.checked ){
-		formulario.reset();
-
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
-
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
-	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-	}
-});
-
-
-const fechaNacimiento = document.getElementById("fechaNacimiento");
-const edad = document.getElementById("edad");
-
-const calcularEdad = (fechaNacimiento) => {
-    const fechaActual = new Date();
-    const anoActual = parseInt(fechaActual.getFullYear());
-    const mesActual = parseInt(fechaActual.getMonth()) + 1;
-    const diaActual = parseInt(fechaActual.getDate());
-
-    // 2016-07-11
-    const anoNacimiento = parseInt(String(fechaNacimiento).substring(0, 4));
-    const mesNacimiento = parseInt(String(fechaNacimiento).substring(5, 7));
-    const diaNacimiento = parseInt(String(fechaNacimiento).substring(8, 10));
-
-    let edad = anoActual - anoNacimiento;
-    if (mesActual < mesNacimiento) {
-        edad--;
-    } else if (mesActual === mesNacimiento) {
-        if (diaActual < diaNacimiento) {
-            edad--;
-        }
-    }
-    return edad;
-};
-
-window.addEventListener('load', function () {
-
-    fechaNacimiento.addEventListener('change', function () {
-        if (this.value) {
-            edad.innerText = `La edad es: ${calcularEdad(this.value)} años`;
-        }
-    });
-
-});
-
-
 document.addEventListener("DOMContentLoaded", function () {
 
-	const enfermedades = document.getElementById('#enfermedades');
-	const enf_si = document.getElementById('#enf_si');
-	const enf_no = document.getElementById('#enf_no');
+    const nombre = document.getElementById('nombre');
+    const apellido = document.getElementById('apellido');
+    const usuario = document.getElementById('usuario');
+    const fe_nac = document.getElementById('fe_nac');
+    const contrasena = document.getElementById('contrasena');
+    const conf_contrasena = document.getElementById('conf_contrasena');
+    const email = document.getElementById('email');
+    const enfermedades = document.getElementById('enfermedades');
+    const enf_si = document.getElementById('enf_si');
+    const enf_no = document.getElementById('enf_no');
+    const edad = document.getElementById('edad_fec');
+    const errorNombre = document.getElementById('error-nombre');
+    const errorApellido = document.getElementById('error-apellido');
+    const errorUsuario = document.getElementById('error-usuario');
+    const errorConf_usuario = document.getElementById('error-conf_usuario');
+    const errorContrasena = document.getElementById('error-contrasena');
+    const errorConf_contrasena = document.getElementById('error-conf_contrasena');
 
-	enf_si.addEventListener('input', function () {
-		validarEnfermedades_si();
-	});
+    nombre.addEventListener('input', function () {
+        validarNombre();
+    });
 
-	enf_no.addEventListener('input', function () {
-			validarEnfermedades_no();
-	});
+    apellido.addEventListener('input', function () {
+        validarApellido();
+    });
 
-	function validarEnfermedades_si() {
-		if (enf_si.value) {
-				enfermedades.classList.add('show');
-	
-		} else {
-				enfermedades.classList.remove('show');
-		}
-	}
+    fe_nac.addEventListener('input', function () {
+        calcularEdad();
+    });
 
-	function validarEnfermedades_no() {
-		if (enf_no.value) {
-				enfermedades.classList.remove('show');
-	
-		} else {
-				enfermedades.classList.add('show');
-		}
-	}
-	
+    usuario.addEventListener('input', function () {
+        validarUsuario();
+    });
+
+    conf_usuario.addEventListener('input', function () {
+        validarConf_usuario();
+    });
+
+    contrasena.addEventListener('input', function () {
+        validarContrasena();
+    });
+
+    conf_contrasena.addEventListener('input', function () {
+        validarConf_contrasena();
+    });
+
+    enf_si.addEventListener('input', function () {
+        validarEnfermedades_si();
+    });
+
+    enf_no.addEventListener('input', function () {
+        validarEnfermedades_no();
+    });
+
+
+    function validarNombre() {
+        if (nombre.value == null || nombre.value == '') {
+            console.log('asdasd');
+            nombre.classList.add('error');
+            errorNombre.classList.add('show');
+        } else {
+            nombre.classList.remove('error');
+            errorNombre.classList.remove('show');
+        }
+    }
+
+    function validarApellido() {
+        if (apellido.value == null || apellido.value == '') {
+            apellido.classList.add('error');
+            errorApellido.classList.add('show');
+        } else {
+            apellido.classList.remove('error');
+            errorApellido.classList.remove('show');
+        }
+    }
+
+    function calcularEdad() {
+        var fechaNacimiento = new Date(fe_nac.value);
+        var fechaActual = new Date();
+        var edadMilisegundos = fechaActual.getTime() - fechaNacimiento.getTime();
+        var edad = Math.floor(edadMilisegundos / (1000 * 60 * 60 * 24 * 365.25));
+        edad_fec.innerHTML = "Usted tiene " + edad + " años.";
+        edad_fec.classList.add('show');
+    }
+
+    function validarUsuario() {
+        const alphanumericRegex = /^[a-zA-Z0-9]+$/;
+        if (!alphanumericRegex.test(usuario.value)) {
+            usuario.classList.add('error');
+            errorUsuario.classList.add('show');
+        } else {
+            usuario.classList.remove('error');
+            errorUsuario.classList.remove('show');
+        }
+    }
+
+
+    function validarConf_usuario() {
+        if (usuario.value !== conf_usuario.value) {
+            conf_usuario.classList.add('error');
+            errorConf_usuario.classList.add('show');
+        } else {
+            conf_usuario.classList.remove('error');
+            errorConf_usuario.classList.remove('show');
+        }
+    }
+
+    function validarContrasena() {
+        const expresion = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])/;
+        if (!expresion.test(contrasena.value)) {
+            contrasena.classList.add('error');
+            errorContrasena.classList.add('show');
+        } else {
+            contrasena.classList.remove('error');
+            errorContrasena.classList.remove('show');
+        }
+    }
+
+    function validarConf_contrasena() {
+        if (contrasena.value !== conf_contrasena.value) {
+            conf_contrasena.classList.add('error');
+            errorConf_contrasena.classList.add('show');
+        } else {
+            conf_contrasena.classList.remove('error');
+            errorConf_contrasena.classList.remove('show');
+        }
+    }
+
+    function validarEnfermedades_si() {
+        if (enf_si.value) {
+            enfermedades.classList.add('show');
+
+        } else {
+            enfermedades.classList.remove('show');
+        }
+    }
+
+    function validarEnfermedades_no() {
+        if (enf_no.value) {
+            enfermedades.classList.remove('show');
+
+        } else {
+            enfermedades.classList.add('show');
+        }
+    }
 })
-
-
